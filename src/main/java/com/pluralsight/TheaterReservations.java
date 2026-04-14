e633a94a12c560287650b6d21f8f528a63ffa23e
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-04-14 14:19:08 -0700

    Initial commit

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class TheaterReservations {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please enter your full name: ");
+    String fullName = scanner.nextLine();
+
+    System.out.println("What date will you be attending (YYYY-MM-DD)? ");
+    String input = scanner.nextLine();
+    LocalDate reservationDate = LocalDate.parse(input);
+
+    System.out.println("How many tickets will you need? ");
+    String numTickets = scanner.nextLine();
+
+
+    // "Thank you" + fullName, +  your ticket is set for + reservation date
+
+
+    if(numTickets.equals("1")) {
+        System.out.println("Thank you " + fullName + " your ticket is dated for " + reservationDate);
+    }
+    else {
+        System.out.println("Thank you " + fullName + " your tickets are dated for " + reservationDate);
+    }
+
+
+        System.out.println(reservationDate);
+        System.out.println(input);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
